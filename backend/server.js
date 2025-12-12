d6f237423352a904cbe3f06bbf7dfbcfd4dfb736
tina123-svg <tinasitaula835@gmail.com>
2025-12-13 00:42:00 +0545

    Implemented user authentication system:Frontend for login and sign up with MongoDb and API for login and signup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./src/routes/authRoutes"));
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log("DB Error:", err));
